47b3a54dbac2a1fbccefe0e8972001736eb01a8e
lbonatoc <luccabonatoc@gmail.com>
2026-08-30 23:52:08 -0300

    feat: criação da view de glossário

---
+++ resources/views/glossario/index.blade.php
@@ -0,0 +1,17 @@
+@extends( 'layouts.app' )
+
+@section( 'title' , 'Glossário | Democracia em Rede' )
+
+@section( 'page' , 'glossario' )
+
+@section( 'content' )
+<main id="app" tabindex="-1">
+    <div class="container page">
+        <div class="skeleton-grid">
+            <div class="skeleton"></div>
+            <div class="skeleton"></div>
+            <div class="skeleton"></div>
+        </div>
+    </div>
+</main>
+@endsection
