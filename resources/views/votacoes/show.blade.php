6cdb20aaab2778055a67603fdf43f5354790609b
lbonatoc <luccabonatoc@gmail.com>
2026-08-30 23:34:32 -0300

    feat: criação da view de visualização de votação

---
+++ resources/views/votacoes/show.blade.php
@@ -0,0 +1,18 @@
+@extends( 'layouts.app' )
+
+@section( 'title' , 'Votação | Democracia em Rede')
+
+@section( 'page' , 'votacao')
+
+@section( 'content' )
+    <main id="app" tabindex="-1">
+        <div class="container page">
+            <div class="skeleton-grid">
+                <div class="skeleton"></div>
+                <div class="skeleton"></div>
+                <div class="skeleton"></div>
+            </div>
+        </div>
+    </main>
+
+@endsection
