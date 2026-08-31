e103a9f23c2620d1895ba3b8ce572e22d6aa4641
lbonatoc <luccabonatoc@gmail.com>
2026-08-31 00:02:20 -0300

    feat: criação da view de comparação de representantes

---
+++ resources/views/comparacao/index.blade.php
@@ -0,0 +1,17 @@
+@extends( 'layouts.app' )
+
+@section( 'title' , 'Comparar Representantes | Democracia em Rede' )
+
+@section( 'page' , 'comparacao' )
+
+@section( 'content' )
+    <main id="app" tabindex="-1">
+        <div class="container page">
+            <div class="skeleton-grid">
+                <div class="skeleton"></div>
+                <div class="skeleton"></div>
+                <div class="skeleton"></div>
+            </div>
+        </div>
+    </main>
+@endsection
